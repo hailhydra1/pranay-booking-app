e8debae7170e51927d55ffc83fb94decbb29cd9c
Pranay <pranaysingh303@gmail.com>
2023-10-31 00:39:58 +0530

    search removed

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -19,11 +19,13 @@ export default function Header() {
         <div>Any week</div> 
         <div className="border-l border-gray-300"></div>
         <div>Add guests</div>
-        <button className="bg-primary text-white p-1 rounded-full">
+        {/* <button className="bg-primary text-white p-1 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
-        </button>
+        </button> */}
+        <div className="border-l border-gray-300"></div>
+        <div>Any Place</div>
       </div>
       <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-red-300 rounded-full shadow-md shadow-red-400 py-2 px-4 ">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
